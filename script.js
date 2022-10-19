3cc6c075f1411d19ceafb332709d164841d15c75
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2022-10-20 00:23:08 +0200

    JS number animation

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counterFormatted) => {
+  counterFormatted.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counterFormatted.getAttribute("data-target");
+    const c = +counterFormatted.innerText;
+    const increment = target / 250;
+    if (c < target) {
+      counterFormatted.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counterFormatted.innerText = target;
+    }
+  };
+  updateCounter();
+});
